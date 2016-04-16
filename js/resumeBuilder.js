68ed456d25ca3e0d2aa2244b925fbc6da6468e48
Vincent Yan <vincentyan1@gmail.com>
2016-04-16 01:41:24 -0700

    Add projects and footer sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -69,78 +69,104 @@ var work = [
 }
 ];
 
-for (x in work){
-
-	$("#workExperience").append(HTMLworkStart);
-	
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work[x].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work[x].title);
-	
-	var formattedWorkDates = HTMLworkDates.replace("%data%", work[x].dates);
-	
-	var formattedWorkLocation = HTMLworkLocation.replace("%data%", work[x].location);
-	
-	var formattedWorkDescription = HTMLworkDescription.replace("%data%", work[x].description)
-
-	$(".work-entry:last").append(formattedEmployer + formattedTitle);
-	$(".work-entry:last").append(formattedWorkDates);
-	$(".work-entry:last").append(formattedWorkLocation);
-	$(".work-entry:last").append(formattedWorkDescription);
-
+function displayWork(){
+	for (x in work){
+		
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work[x].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work[x].title);
+		
+		var formattedWorkDates = HTMLworkDates.replace("%data%", work[x].dates);
+		
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work[x].location);
+		
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%", work[x].description);
+		$("#workExperience").append(HTMLworkStart);
+		$(".work-entry:last").append(formattedEmployer + formattedTitle);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedWorkDescription);
+	}	
 }
+
+displayWork();
 
 /* PROJECTS */
 
-var projects = [
-{
-	"title" : "Giant Bomb API Hack",
-	"date" : "February 2016",
-	"description" : "This App allows user to type a title of a game or game series and retrieves data about release date, box art and brief synopsis of title.",
-	"image" : ""
-},
-{
-	"title" : "StackOverflow API App",
-	"date" : "January 2016",
-	"description" : "This app allows you to displays the first(most recent) 30 unanswered questions for a tag entered by the user. It also displays 20 top answerers for a particular tag entered by the user.",
-	"image" : ""
-},
-{
-	"title" : "Quiz App",
-	"date" : "January 2016",
-	"description" : "The app presents 5 questions to the user and displays the score at the end of the quiz.",
-	"image" : ""
-},
-{
-	"title" : "Hot or Cold Game",
-	"date" : "December 2015",
-	"description" : "The app selects a random secret number between 1 and 100 and keeps it hidden. The player should guess the number within the number of guesses provided. The player will get feedback on how close ('hot'​) or far ('cold') the guess is.",
-	"image" : ""
-},
-{
-	"title" : "Shopping List App",
-	"date" : "December 2015",
-	"description" : "The app allows you to type an item and append it to list and can rearrange items in any order.",
-	"image" : ""
-},
-{
-	"title" : "FizzBuzz",
-	"date" : "November 2015",
-	"description" : "This app counts from 1 to the number entered by the user. For every number that isn't divisible by 3 or 5, it simply displays the number. If the number is divisible by 3, it displays 'Fizz'. If it is divisible by 5, it displays 'Buzz​'. And finally if it's divisible by both 3 and 5, it displays 'FizzBuzz'.",
-	"image" : ""
-},
-{
-	"title" : "Street Fighter Game",
-	"date" : "November 2015",
-	"description" : "It brings together DOM manipulation, events and effects from the jQuery API thereby building a themed interactive web page.",
-	"image" : ""
-},
-{
-	"title" : "Karma Landing Page Clone",
-	"date" : "October 2015",
-	"description" : "This is a clone of the landing page of a startup called Karma. Since Karma is a live startup that regularly updates their site, the link below is of a previous version of Karma's site which is what my clone looks like.",
-	"image" : ""
-}
-]
+var projects = {
+	"projects" : [
+	{
+		"title" : "Giant Bomb API Hack",
+		"date" : "February 2016",
+		"description" : "This App allows user to type a title of a game or game series and retrieves data about release date, box art and brief synopsis of title.",
+		"images" : ["images/api-hack.png"]
+	},
+	{
+		"title" : "StackOverflow API App",
+		"date" : "January 2016",
+		"description" : "This app allows you to displays the first(most recent) 30 unanswered questions for a tag entered by the user. It also displays 20 top answerers for a particular tag entered by the user.",
+		"images" : ["images/stackoverflow.png"]
+	},
+	{
+		"title" : "Quiz App",
+		"date" : "January 2016",
+		"description" : "The app presents 5 questions to the user and displays the score at the end of the quiz.",
+		"images" : ["images/quiz-app.png"]
+	},
+	{
+		"title" : "Hot or Cold Game",
+		"date" : "December 2015",
+		"description" : "The app selects a random secret number between 1 and 100 and keeps it hidden. The player should guess the number within the number of guesses provided. The player will get feedback on how close ('hot'​) or far ('cold') the guess is.",
+		"images" : ["images/hot-or-cold.png"]
+	},
+	{
+		"title" : "Shopping List App",
+		"date" : "December 2015",
+		"description" : "The app allows you to type an item and append it to list and can rearrange items in any order.",
+		"images" : ["images/shopping-list.png"]
+	},
+	{
+		"title" : "FizzBuzz",
+		"date" : "November 2015",
+		"description" : "This app counts from 1 to the number entered by the user. For every number that isn't divisible by 3 or 5, it simply displays the number. If the number is divisible by 3, it displays 'Fizz'. If it is divisible by 5, it displays 'Buzz​'. And finally if it's divisible by both 3 and 5, it displays 'FizzBuzz'.",
+		"images" : ["images/fizzbuzz.png"]
+	},
+	{
+		"title" : "Street Fighter Game",
+		"date" : "November 2015",
+		"description" : "It brings together DOM manipulation, events and effects from the jQuery API thereby building a themed interactive web page.",
+		"images" : ["images/street-fighter.png"]
+	},
+	{
+		"title" : "Karma Landing Page Clone",
+		"date" : "October 2015",
+		"description" : "This is a clone of the landing page of a startup called Karma. Since Karma is a live startup that regularly updates their site, the link below is of a previous version of Karma's site which is what my clone looks like.",
+		"images" : ["images/karma.png"]
+	}
+]};
+
+projects.display = function(){
+	for (project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedProjectTitle);
+
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].date);
+		$(".project-entry:last").append(formattedProjectDates);
+
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+
+	 if (projects.projects[project].images.length > 0) {
+     for (image in projects.projects[project].images) {
+       var formmattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+       $(".project-entry:last").append(formmattedProjectImage);
+     }
+   }
+	}
+};
+
+projects.display();
 
 /* EDUCATION */
 
@@ -165,3 +191,25 @@ var education = {
 	}
 	]
 }
+
+$("#education").append(HTMLschoolStart);
+
+var formattedEducationName = HTMLschoolName.replace("%data%", education.schools.name);
+var formattedEducationLocation = HTMLschoolLocation.replace("%data%", education.schools.location);
+var formattedEducationMajor = HTMLschoolDegree.replace("%data%", education.schools.major);
+var formattedEducationDates = HTMLschoolDates.replace("%data%", education.schools.dates);
+
+$(".education-entry:last").append(formattedEducationName + formattedEducationLocation + formattedEducationMajor + formattedEducationDates);
+
+$(".education-entry:last").append(HTMLonlineClasses);
+
+/*for (online in education){
+	var formattedOnlineName = HTMLschoolName.replace("%data%", education.schools.name);
+	var formattedEducationLocation = HTMLschoolLocation.replace("%data%", education.schools.location);
+	var formattedEducationMajor = HTMLschoolDegree.replace("%data%", education.schools.major);
+	var formattedEducationDates = HTMLschoolDates.replace("%data%", education.schools.dates);
+}*/
+
+/* FOOTER */
+
+$("#footerContacts").append(formattedMobile + formattedEmail + formattedTwitter + formattedGitHub + formattedLocation);
